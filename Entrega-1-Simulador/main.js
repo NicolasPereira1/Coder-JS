8142391a23aa333ea55e45fbe14f111b0a5d1ade
Nicolas <nicopere1998@gmail.com>
2025-06-11 22:45:24 -0300

    agrego contador de intentos restantes

--- Entrega-1-Simulador/main.js
+++ Entrega-1-Simulador/main.js
@@ -1,59 +1,68 @@
-//constantes de numeros maximos y mínimos entre 1-100
-const MIN = 1;
-const MAX = 100;
+  //constantes de numeros máximos y mínimos entre 1-100
+  const MIN = 1;
+  const MAX = 100;
 
-//funcion numero aleatorio
-function numeroGenerado() {
-  return Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
-}
-
-//funcion solicito numero del jugador
-function pedirNumero() {
-  return parseInt(prompt("Adivina el número del 1-100: "));
-}
-
-//funcion si quiere jugar otra vez una vez terminado el juego
-  function jugarOtraVez() {
-  const respuesta = prompt("¿Quieres jugar otra vez? (si/no)").toLowerCase();
-
-  if (respuesta === "si" || respuesta === "sí") {
-    juego();
-  } 
-  else if (respuesta === "no") {
-    alert("Juego terminado. ¡Gracias por jugar!");
-  } 
-  else {
-    alert("Respuesta inválida. Responde 'si' o 'no'.");
-    return jugarOtraVez(); // <--- importante el return aquí
+  //funcion numero aleatorio
+  function numeroGenerado() {
+    return Math.floor(Math.random() * (MAX - MIN + 1)) + MIN; 
   }
-}
 
-// defino funcion del juego
-function juego() {
-  const numeroCorrecto = numeroGenerado();
-  let intentos = 1;
-  let numeroElegido = pedirNumero();
+  //funcion solicito numero del jugador
+  function pedirNumero() {
+    return parseInt(prompt("Adivina el número del 1-100:  "));
+  }
 
-  while (numeroElegido !== numeroCorrecto) {
-    if (numeroElegido > numeroCorrecto && numeroElegido <= 100) {
-      alert("Muy alto. Intenta de nuevo. Intentos: " + intentos++);
+  //funcion si quiere jugar otra vez una vez terminado el juego
+    function jugarOtraVez() {
+    const respuesta = prompt("¿Quieres jugar otra vez? (si/no)").toLowerCase();
+
+    if (respuesta === "si" || respuesta === "sí") {
+      juego();
     } 
-    else if (numeroElegido < numeroCorrecto && numeroElegido >= 1) {
-      alert("Muy bajo. Intenta de nuevo. Intentos: " + intentos++);
-    } 
-    else if (numeroElegido > 100 || numeroElegido < 1 || isNaN(numeroElegido)) {
-      alert("Introduce un número válido entre 1 y 100. Intentos: " + intentos++);
+    else if (respuesta === "no") {
+      alert("Juego terminado. ¡Gracias por jugar!");
     } 
     else {
-      alert("Número incorrecto. Intenta de nuevo. Intentos: " + intentos++);
+      alert("Respuesta inválida. Responde 'si' o 'no'.");
+      return jugarOtraVez();
     }
-
-    numeroElegido = pedirNumero();
   }
 
-  alert("¡Correcto! El número era " + numeroCorrecto + ". Lo lograste en " + intentos + " intentos.");
-  jugarOtraVez();
-}
-//inicio juego por fuera de la funcion para que se ejecute al cargar la pagina
-juego();
+  // defino funcion del juego
+  function juego() {
+    const numeroCorrecto = numeroGenerado();
+    let intentos = 0;
+    let numeroElegido = pedirNumero();
+    let intentosMaximos = 5;
+
+    while (numeroElegido !== numeroCorrecto && intentos < intentosMaximos) {
+      if (numeroElegido > numeroCorrecto && numeroElegido <= 100) {
+        alert("Muy alto. Intenta de nuevo. Intentos restantes: " + (intentosMaximos - intentos - 1));
+        intentos++;
+      } 
+      else if (numeroElegido < numeroCorrecto && numeroElegido >= 1) {
+        alert("Muy bajo. Intenta de nuevo. Intentos restantes: " + (intentosMaximos - intentos - 1));
+        intentos++;
+      } 
+      else if (isNaN(numeroElegido) || numeroElegido > 100 || numeroElegido < 1) {
+        alert("Introduce un número válido entre 1 y 100.");
+      }
+      
+      if (intentos < intentosMaximos) {
+       numeroElegido = pedirNumero();
+      }
+      
+      
+    }
+
+    if (numeroCorrecto === numeroElegido) {
+    alert("Ganaste! El número era " + numeroCorrecto + ". Lo lograste en " + intentos + " intentos.");
+    }
+    else {
+      alert("Perdiste! No te quedan mas intentos.  El número era " + numeroCorrecto + ".");
+    }
+    jugarOtraVez();
+  }
+  //inicio juego por fuera de la funcion para que se ejecute al cargar la pagina automaticamente
+  juego();
 
